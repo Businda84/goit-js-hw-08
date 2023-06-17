dbc47332786722b0f862ce69f0db6cbb5e942261
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-06-17 18:55:23 +0300

    commit 17.06

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
 
+//  id="vimeo-player"
+//       src="https://player.vimeo.com/video/236203659"
+//       width="640"
+//       height="360"
+//       frameborder="0"
+//       allowfullscreen
+//       allow="autoplay; encrypted-media"
+
+
+
+    const player = new Player(iframe);
+    const iframe = document.querySelector('#vimeo-player');
+    const onPlay = function(data) {
+   
+    duration: 61.857
+    percent: 0.049
+    seconds: 3.034
+
+};
+
+player.on('play', onPlay);
